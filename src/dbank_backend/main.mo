162d82b89edddd697d2e464343bd515fafb4237a
jbuican14 <jbuican19@gmail.com>
2024-11-24 22:10:23 +0000

    DFX-simple e-wallet to add and withdraw amount

--- src/dbank_backend/main.mo
+++ src/dbank_backend/main.mo
@@ -1,5 +1,50 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+// actor {
+//   public query func greet(name : Text) : async Text {
+//     return "Hello, " # name # "!";
+//   };
+// };
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor Dbank {
+  var currentValue : Float = 300;
+  currentValue := 300;
+  let id = 374655738277;
+  // Debug.print(debug_show (id));
+  // type Time = Int;
+  stable var startTime = Time.now();
+  Debug.print(debug_show (startTime));
+
+  // deposit crypto
+  public func topUp(amount : Float) {
+    // Natural number
+    currentValue += amount;
+    Debug.print(debug_show (currentValue));
+  };
+  // topUp();df
+
+  public func withdraw(amount : Float) {
+    if (amount <= currentValue) {
+      currentValue := currentValue - amount;
+      Debug.print("Withdraw...");
+      Debug.print(debug_show (currentValue));
+    } else {
+      Debug.print("You do not have sufficient fund");
+    };
+  };
+
+  // read only operation to prevent the issue loading
+  public query func checkBalance() : async Float {
+    return currentValue;
+  };
+
+  public func compound() {
+    let currentTime = Time.now();
+    let timeElapsedNS = currentTime - startTime;
+    let timeElapsedS = timeElapsedNS / 1000000000;
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedS));
+
+    startTime := currentTime;
   };
 };
--- src/dbank_frontend/src/App.jsx
+++ src/dbank_frontend/src/App.jsx
@@ -1,29 +1,111 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { dbank_backend } from 'declarations/dbank_backend';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [balance, setBalance] = useState(0);
+  const [topUpAmt, setTopUpAmt] = useState(0);
+  const [withdraw, setWithdraw] = useState(0);
+  const [value, setValue] = useState('');
+
+  const checkBal = async () => {
+    const bal = await dbank_backend.checkBalance();
+    return bal;
+  };
+
+  useEffect(() => {
+    checkBal().then((bal) => {
+      setBalance(bal);
+      setValue(true)
+    });
+  }, []);
+
+  function handleClear(t) {
+    setValue(t)
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
-    const name = event.target.elements.name.value;
-    dbank_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
+    handleClear(false);
+
+    // read form data
+    const form = event.target;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+
+    const inputAmt = parseFloat(formJson.topUp) || 0;
+    const outputAmt = parseFloat(formJson.withdraw) || 0;
+    if(inputAmt === 0 && outputAmt === 0) {
+      handleClear(true);
+    }
+
+    if (inputAmt > 0) {
+      dbank_backend.topUp(inputAmt).then((e) => {
+        // setGreeting(greeting);
+        checkBal().then((b) => {
+          setBalance(b);
+          setTopUpAmt(0)
+          handleClear(true)
+        });
+      });
+    }
+
+    if (outputAmt > 0) {
+      dbank_backend.withdraw(outputAmt).then((e) => {
+        checkBal().then((b) => {
+          setBalance(b)
+          setWithdraw(0);
+          handleClear(true)
+        });
+      });
+    }
   }
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
+    <main className="container">
+      <img
+        className="logo-dwallet"
+        src="default.png"
+        alt="DBank logo"
+        width="100"
+      />
       <br />
       <br />
+      <h1>
+        Current Balance: $
+        <span id="value">{balance ? balance.toFixed(2) : ''}</span>
+      </h1>
+      <div className="divider"></div>
+
       <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
+        <h2>Amount to Top Up</h2>
+        <label htmlFor="input-amount">
+          <input
+            id="input-amount"
+            type="number"
+            onChange={(e) => setTopUpAmt(e.target.value)}
+            step="0.01"
+            min={0}
+            name="topUp"
+            value={topUpAmt}
+          />
+        </label>
+        <h2>Amount to Withdraw</h2>
+        <label htmlFor="withdrawal-amount">
+          <input
+            id="withdrawal-amount"
+            type="number"
+            onChange={(e) => setWithdraw(e.target.value)}
+            name="withdraw"
+            step="0.01"
+            min={0}
+            value={withdraw}
+          />
+        </label>
+
+        <button id="submit-btn" type="submit" disabled={!value}>
+          Finalise Transaction
+        </button>
       </form>
-      <section id="greeting">{greeting}</section>
     </main>
   );
 }
